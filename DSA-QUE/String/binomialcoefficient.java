a43216f1f6577e890640c68f8ee901d0a23f1069
Pritish Mandal <pritishmandal21@gmail.com>
2026-03-06 23:16:29 +0530

    function method

---
+++ DSA-QUE/String/binomialcoefficient.java
@@ -0,0 +1,25 @@
+public class binomialcoefficient {
+   
+    public static int factorial(int num) {//code throuth gPt
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        return fact;
+    } 
+   public static int bincoeff(int n, int r){
+        int fact_n= factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr= factorial(n-r);
+        
+        int bincoeff=fact_n/(fact_r*fact_nmr);
+        return bincoeff;
+    
+    }
+    public static void main(String args[]) {
+        System.out.println(bincoeff(5,2));
+        
+    }
+}
+
+
