5bd5b916cffa43e56768b7282fe5ae8a02ad6d8d
Pritish Mandal <pritishmandal21@gmail.com>
2026-03-08 22:54:23 +0530

    method

--- DSA-QUE/String/binomialcoefficient.java
+++ DSA-QUE/String/binomialcoefficient.java
@@ -5,12 +5,12 @@ public class binomialcoefficient {
         for (int i = 1; i <= num; i++) {
             fact *= i;
         }
-        return fact;
+        return fact;//return fact;
     } 
    public static int bincoeff(int n, int r){
         int fact_n= factorial(n);
         int fact_r = factorial(r);
-        int fact_nmr= factorial(n-r);
+        int fact_nmr= factorial(n-r);//n-r  
         
         int bincoeff=fact_n/(fact_r*fact_nmr);
         return bincoeff;
--- DSA-QUE/String/characterfrequency.java
+++ DSA-QUE/String/characterfrequency.java
@@ -9,7 +9,7 @@ public class characterfrequency {
 
         for (int i = 0; i < 256; i++) {
             if (freq[i] != 0) {
-                System.out.println((char) i + " : " + freq[i]);
+                System.out.println((char) i + " : " + freq[i]);// print character and its frequency
             }
         }
     }
