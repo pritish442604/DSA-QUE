a2188632b67463e79444d762a5e1ad63bd32e156
Pritish Mandal <pritishmandal21@gmail.com>
2026-03-14 20:57:14 +0530

    function method questions

--- DSA-QUE/String/removeduplicate.java
+++ DSA-QUE/String/removeduplicate.java
@@ -2,17 +2,17 @@ public class removeduplicate {
 
     public static void main(String[] args) {
 
-        String str = "programming";
+        String str = "programming";// define a string and initialize an empty result string to store unique characters
         String result = "";
 
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
 
-            if (result.indexOf(ch) == -1) {
+            if (result.indexOf(ch) == -1) {// check if the character is not already in the result string
                 result += ch;
             }
         }
 
-        System.out.println("After removing duplicates: " + result);
+        System.out.println("After removing duplicates: " + result);// print the string after removing duplicates
     }
 }
