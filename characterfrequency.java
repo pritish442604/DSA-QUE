70ffbd3e436d2a89941568b5bf76db685e5a0fa9
Pritish Mandal <pritishmandal21@gmail.com>
2026-03-21 21:33:57 +0530

    .

--- characterfrequency.java
+++ characterfrequency.java
@@ -3,7 +3,7 @@ public class characterfrequency {
         String str = "banana";
         int[] freq = new int[256];
 
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {    // loop through each character in the string
             freq[str.charAt(i)]++;
         }
 
@@ -12,5 +12,5 @@ public class characterfrequency {
                 System.out.println((char) i + " : " + freq[i]);// print character and its frequency
             }
         }
-    }
+    }// for loop to count frequency of characters in the string and print the result    
 }//
